56cde285bf91fd1492c82d5f4aad96706ccde30e
YouLikeCats <thisisbryanchan@gmail.com>
2021-01-13 22:47:05 -0500

    Added Level Command

---
+++ commands/mylevel.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+const sql = require('sqlite3').verbose();
+var db = new sql.Database("db.sqlite");
+const {prefix, token, status, gatewaychannelid, modlogchannelid, messagechannelid} = require("../config.json"); //get the prefix, token, status and welcome channel id
+
+exports.run = (client, message, args, level) => {
+    return message.channel.send(`You are level ${level}!`);
+}
+
+exports.config = {
+  name: "mylevel",
+  usage: "mylevel",
+  description: "Get your bot permission level!",
+  category: "misc",
+  permissionLevel: 0
+};
